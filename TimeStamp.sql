bcce5cbb14a378be236b41642ce5d95fef1157c6
jenoch <jenoch@JENOCH01>
2018-04-10 16:10:38 -0400

    fixed two queries that were not exactly precise

--- TimeStamp.sql
+++ TimeStamp.sql
@@ -8,6 +8,6 @@ SELECT [ID]
       ,[AmountChanged]
       ,[newAmount]
   FROM [dbo].[Transactions]
-WHERE Timestamp = '2018-04-10 12:00:00'
+WHERE Timestamp > '2018-04-09 12:59:00'
 
 
---
+++ timestamptop10.sql
@@ -0,0 +1,8 @@
+USE [BankTransactions]
+GO
+
+SELECT TOP 10 *
+  FROM [dbo].[Transactions]
+WHERE Timestamp > '2018-04-09 12:59:00'
+
+
